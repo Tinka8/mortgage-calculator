65e2b9aa1fae29f238e92e7466e1432c14db523b
Tinka8 <lenmatka818@gmail.com>
2023-06-12 16:43:17 +0200

    index

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,48 @@
     <div class="bg-gray-100 min-h-screen w-full h-full p-12">
         <div class="container mx-auto bg-purple-100 shadow-md rounded p-10 m-12">
             <h1 class="font-bold text-xl pb-10">Mortgage calculator</h1>
-            <div class="grid gap-y-72 grid-cols-3 text-lg">
-                <div class="flex items-center">
-                    <div>Purchase price:</div>
-                    <div class="font-semibold text-lg">$450,000</div>
-                    <div></div>
+            <div class="grid grid-cols-3 gap-10 text-lg">
+                <div>
+                    <div>Purchase price: <span class="font-semibold text-xl">${{ purchasePrice }}</span></div>
+                    <input v-model="purchasePrice" placeholder="Purchase Price" />
                 </div>
-                <div class="flex items-center">
-                    <div>Down payment:</div> 
-                    <div class="font-semibold text-lg">$135,000</div>
-                    <div></div>
+                <div>
+                    <div>Down payment: <span class="font-semibold text-xl">${{ downPayment }}</span></div> 
+                    <input v-model="downPayment" placeholder="Down Payment" />
                 </div>
-                <div class="flex items-center">
-                    <div>Repayment time:</div> 
-                    <div class="font-semibold text-lg">25 years</div>
-                    <div></div>
+                <div>
+                    <div>Repayment time: <span class="font-semibold text-xl">{{ repaymentTime }} years</span></div> 
+                    <input v-model="repaymentTime" placeholder="Repayment Time" />
                 </div>
                 <div class="flex items-center">
                     <div>Interest rate:</div> 
-                    <div class="font-semibold text-lg">3%</div>
+                    <div class="font-semibold text-xl">3%</div>
                     <div></div>
                 </div>
                 <div>
                     <div>Loan amount</div>
-                    <div class="font-semibold text-lg"> $315,000</div>
+                    <div class="font-semibold text-xl"> $315,000</div>
                 </div>
                 <div>
                     <div>Estimated pr. month:</div>
-                    <div class="font-semibold text-lg">$1,300</div>
+                    <div class="font-semibold text-xl">$1,300</div>
                 </div>
             </div>
-            <button class="bg-purple-600 hover:bg-purple-700 shadow-xl text-white text-sm font-bold px-2 py-2 rounded w-1/6 my-8">Get a mortgage quote</button>
+            <button class="bg-purple-600 hover:bg-purple-700 shadow-xl text-white text-sm font-bold px-2 py-4 rounded w-1/6 my-8">Get a mortgage quote</button>
         </div>
     </div>
 </template>
 
 <script setup> 
+import { ref } from 'vue';
+
+const numebrs = ref({
+    purchasePrice: "",
+    downPayment: "",
+    repaymentTime: "",
+    interestRate: ""
+});
+
+
+
 </script>
