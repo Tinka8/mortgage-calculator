c29475a7fc41a9adb3b5134afd68cb0b238ea3ef
Tinka8 <lenmatka818@gmail.com>
2023-06-14 13:47:02 +0200

    index

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-    <div class="bg-gray-100 min-h-screen w-full h-full p-12">
+    <div class="bg-gray-100 min-h-screen w-full h-full p-12 text-gray-600">
         <div class="container mx-auto bg-purple-100 shadow-md rounded p-10 m-12">
-            <h1 class="font-bold text-xl pb-10">Mortgage calculator</h1>
+            <h1 class="font-bold text-3xl pb-10">Mortgage calculator</h1>
             <div class="grid grid-cols-3 gap-10 text-lg">
                 <div>
                     <div>Purchase price: <span class="font-semibold text-xl">${{ numbers.purchasePrice }}</span></div>
@@ -17,15 +17,15 @@
                 </div>
                 <div class="flex items-center">
                     <div>Interest rate:</div> 
-                    <div class="font-semibold text-xl">3%</div>
+                    <div class="font-semibold text-xl text-black">3%</div>
                 </div>
                 <div>
                     <div>Loan amount</div>
-                    <div class="font-semibold text-xl"> $315,000</div>
+                    <div class="font-semibold text-xl text-black"> $315,000</div>
                 </div>
                 <div>
                     <div>Estimated pr. month:</div>
-                    <div class="font-semibold text-xl">$1,300</div>
+                    <div class="font-semibold text-xl text-black">${{ estimatedPerMonth.toFixed(2) }}</div>
                 </div>
             </div>
             <button class="bg-purple-600 hover:bg-purple-700 shadow-xl text-white text-sm font-bold px-2 py-4 rounded w-1/6 my-8">Get a mortgage quote</button>
@@ -34,7 +34,7 @@
 </template>
 
 <script setup> 
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 
 const numbers = ref({
     purchasePrice: "",
@@ -43,7 +43,10 @@ const numbers = ref({
     interestRate: ""
 });
 
-// purchase price a down payment
+
+const estimatedPerMonth = computed(() => {
+    return numbers.value.purchasePrice / 12;
+});
 
 
 </script>
